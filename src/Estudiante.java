f2182a2aaf2fac4e1af0db244e9e3c2ad01a059e
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-17 15:46:52 -0300

    feat: implements estudiante class

---
+++ src/Estudiante.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Estudiante extends Persona{
+    public ArrayList<Materia> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(ArrayList<Materia> materias) {
+        this.materias = materias;
+    }
+
+    public ArrayList<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(ArrayList<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public void inscribirCurso(Curso curso) {
+        
+    }
+
+    public void inscribirMateria(Materia materia) {
+
+    }
+
+    private ArrayList<Materia> materias;
+    private ArrayList<Curso> cursos;
+
+    Estudiante(int dni, String nombre) {
+        super(dni, nombre);
+        setMaterias(new ArrayList<Materia>());
+        setCursos(new ArrayList<Curso>());
+    }
+}
